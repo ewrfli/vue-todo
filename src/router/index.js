bb9a981a844d0c0a813b778fb7d0597712ed12c8
ewrfli <q304062@163.com>
2021-12-15 18:11:43 +0800

    feat: add router

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,7 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 import HelloWorld from '@/components/HelloWorld'
+import test01 from '@/components/test01'
 
 Vue.use(Router)
 
@@ -10,6 +11,10 @@ export default new Router({
       path: '/',
       name: 'HelloWorld',
       component: HelloWorld
+    },
+    {
+      path: "/test01",
+      component: test01
     }
   ]
 })
